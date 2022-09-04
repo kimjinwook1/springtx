8fd9c81396d0fca1a18c149f075d54a06b1abe9e
kimjinwook1 <rlawlsdnr628@naver.com>
2022-09-04 13:50:07 +0900

    트랜잭션 전파 활용 - REQUIRED, REQUIRED_NEW

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.UnexpectedRollbackException;
 
 @Slf4j
 @SpringBootTest
@@ -109,6 +110,43 @@ class MemberServiceTest {
 
 		//then: 모든 데이터가 롤백된다.
 		assertTrue(memberRepository.find(username).isEmpty());
+		assertTrue(logRepository.find(username).isEmpty());
+	}
+
+	/**
+	 * memberService	@Transactional:ON
+	 * memberRepository	@Transactional:ON
+	 * logRepository	@Transactional:ON Exception
+	 */
+	@Test
+	void recoverException_fail() {
+		//given
+		String username = "로그예외_recoverException_fail";
+
+		//when
+		assertThatThrownBy(() -> memberService.joinV2(username))
+				.isInstanceOf(UnexpectedRollbackException.class);
+
+		//then: 모든 데이터가 롤백된다.
+		assertTrue(memberRepository.find(username).isEmpty());
+		assertTrue(logRepository.find(username).isEmpty());
+	}
+
+	/**
+	 * memberService	@Transactional:ON
+	 * memberRepository	@Transactional:ON
+	 * logRepository	@Transactional:ON(REQUIRES_NEW) Exception
+	 */
+	@Test
+	void recoverException_success() {
+		//given
+		String username = "로그예외_recoverException_success";
+
+		//when
+		memberService.joinV2(username);
+
+		//then: member 저장, log 롤백.
+		assertTrue(memberRepository.find(username).isPresent());
 		assertTrue(logRepository.find(username).isEmpty());
 	}
 
